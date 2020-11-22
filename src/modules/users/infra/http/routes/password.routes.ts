1721c3caa88420dfaf785fc0c9e01a3cde13cae3
hugo miranda <hugo.miranda.souza@gmail.com>
2020-11-21 21:08:45 -0300

    feat: Forgot password

--- src/modules/users/infra/http/routes/password.routes.ts
+++ src/modules/users/infra/http/routes/password.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
+import ForgotPasswordController from '../controllers/ForgotPasswordController';
 import ResetPasswordController from '../controllers/ResetPasswordController';
 
 const passwordRouter = Router();
+const forgotController = new ForgotPasswordController();
 const resetPasswordController = new ResetPasswordController();
 
+passwordRouter.post('/forgot', forgotController.create);
 passwordRouter.post('/reset', resetPasswordController.create);
 
 export default passwordRouter;
