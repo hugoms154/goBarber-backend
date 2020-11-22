8b9872acda0fde9eec674402851497e7725d990b
hugo miranda <hugo.miranda.souza@gmail.com>
2020-11-21 21:00:09 -0300

    Feat: reset password

---
+++ src/modules/users/infra/http/routes/password.routes.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+import ResetPasswordController from '../controllers/ResetPasswordController';
+
+const passwordRouter = Router();
+const resetPasswordController = new ResetPasswordController();
+
+passwordRouter.post('/reset', resetPasswordController.create);
+
+export default passwordRouter;
