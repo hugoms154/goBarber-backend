ff03c81ccd8ea8cf2742da42e030f9bc34fc88af
hugo miranda <hugo.miranda.souza@gmail.com>
2020-12-03 18:59:02 -0300

    chore: import dotenv config

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,6 +2,7 @@ import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import { errors } from 'celebrate';
 
+import 'dotenv/config';
 import 'express-async-errors';
 import '@shared/infra/typeorm';
 import '@shared/container';
