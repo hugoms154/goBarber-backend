d59736d46d0216466db86830b8d601917f29a2d7
hugo miranda <hugo.miranda.souza@gmail.com>
2020-12-03 15:44:04 -0300

    feature: List Provider Appointments

---
+++ src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
@@ -0,0 +1,37 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import ListProviderAppointmentsService from './ListProviderAppointmentsService';
+
+let fakeAppointmentsRespository: FakeAppointmentsRepository;
+let listProviderAppointments: ListProviderAppointmentsService;
+
+describe('ListProviderAppointments', () => {
+  beforeEach(() => {
+    fakeAppointmentsRespository = new FakeAppointmentsRepository();
+    listProviderAppointments = new ListProviderAppointmentsService(
+      fakeAppointmentsRespository,
+    );
+  });
+
+  it('should be able to list the appointments of a specific day', async () => {
+    const appointment1 = await fakeAppointmentsRespository.create({
+      provider_id: 'provider',
+      user_id: 'user2',
+      date: new Date(2020, 4, 20, 11, 0, 0),
+    });
+
+    const appointment2 = await fakeAppointmentsRespository.create({
+      provider_id: 'provider',
+      user_id: 'user2',
+      date: new Date(2020, 4, 20, 15, 0, 0),
+    });
+
+    const appointments = await listProviderAppointments.execute({
+      provider_id: 'provider',
+      day: 20,
+      month: 5,
+      year: 2020,
+    });
+
+    expect(appointments).toEqual([appointment1, appointment2]);
+  });
+});
