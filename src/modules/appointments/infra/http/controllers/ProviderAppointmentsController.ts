01e9113bc01df02211efdf6561a3232ddaf6e053
hugo miranda <hugo.miranda.souza@gmail.com>
2020-12-18 12:17:17 -0300

    fix: get params from request query

--- src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
@@ -1,22 +1,23 @@
 import ListProviderAppointmentsService from '@modules/appointments/services/ListProviderAppointmentsService';
+import { classToClass } from 'class-transformer';
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
 export default class ProviderAppointmentsController {
   public async index(request: Request, response: Response): Promise<Response> {
     const provider_id = request.user.id;
-    const { day, month, year } = request.body;
+    const { day, month, year } = request.query;
     const listProviderAppointments = container.resolve(
       ListProviderAppointmentsService,
     );
 
-    const availability = await listProviderAppointments.execute({
+    const appointments = await listProviderAppointments.execute({
       provider_id,
-      day,
-      month,
-      year,
+      day: Number(day),
+      month: Number(month),
+      year: Number(year),
     });
 
-    return response.json(availability);
+    return response.json(classToClass(appointments));
   }
 }
